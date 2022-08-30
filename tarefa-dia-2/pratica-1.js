16ee4dba9118bb4457463a63e6f4f201245bdeab
Rogerio M Dias <rogerio_md@hotmail.com>
2022-08-30 14:54:56 -0300

    atualizacao consolelog

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,16 +2,10 @@ let nome = "Rogerio"
 let email = "rogerio_md@hotmail.com"
 let profissao = "Analista"
 let idade = 38
-let pais = "Brasil"
+let pais = "Suiça"
 
 console.log(`Nome: ${nome}`)
-console.log(email)
+console.log(`E-mail: ${email}`)
 console.log(profissao)
 console.log(idade)
 console.log(pais)
-
-//nome = "Rogerio"
-//email = "rogerio_md@hotmail.com"
-//profissao = "Analista"
-//idade = 38
-//pais = "Suiça"
